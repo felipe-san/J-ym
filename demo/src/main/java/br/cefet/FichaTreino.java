d9fb86d84fb97dd93c1275c610dc0118b3a1e360
feefsan <twelve.feh@gmail.com>
2025-04-09 23:30:26 -0300

    Add attributes, getter/setter methods and constructor method

--- demo/src/main/java/br/cefet/FichaTreino.java
+++ demo/src/main/java/br/cefet/FichaTreino.java
@@ -1,4 +1,21 @@
 package br.cefet;
-public class FichaTreino {
 
+import java.util.List;
+import java.util.ArrayList;
+
+public class FichaTreino {
+    private String nome;
+    private List<Exercicio> exercicios;
+
+    public FichaTreino(){
+        exercicios = new ArrayList<>();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getExercicios(){
+        return exercicios;
+    }
 }
