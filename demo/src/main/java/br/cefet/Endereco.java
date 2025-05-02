e792d0d6a2bdf9c3fd84c116e1ac5bfd99930588
feefsan <twelve.feh@gmail.com>
2025-05-01 23:19:33 -0300

    Code Endereco and implement it

--- demo/src/main/java/br/cefet/Endereco.java
+++ demo/src/main/java/br/cefet/Endereco.java
@@ -1,5 +1,15 @@
 package br.cefet;
 
 public class Endereco {
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String estado;
 
+    public Endereco(){
+        
+    }
 }
