12263953664c1f8b0f72e2349e44047632d76ea8
feefsan <twelve.feh@gmail.com>
2025-05-02 23:44:48 -0300

    Adequate Aluno class to inherit attributes from Pessoa

--- demo/src/main/java/br/cefet/Aluno.java
+++ demo/src/main/java/br/cefet/Aluno.java
@@ -1,133 +1,24 @@
 package br.cefet;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Aluno {
-    private String nome;
-    private String cpf;
-    private String endereco;
-    private int matricula;
-    private String dataNascimento;
-    private String genero;
-    private float peso;
-    private float altura;
-    private String email;
-    private List<String> telefones;
-    private float imc;
-    private Plano plano;
-
-    public Aluno(String nome, int matricula, String dataNascimento){
-        this.telefones = new ArrayList<>();
-        setNome(nome);
-        setMatricula(matricula);
-        setDataNascimento(dataNascimento);
-    }
-
-    public Plano getPlano(){
-        return plano;
-    }
-
-    public String getNome(){
-        return nome; 
-    }
+public class Aluno extends Pessoa{
+    private Assinatura assinatura;
     
-    public String getCpf(){
-        return cpf; 
+    // private String nome; // vai pra pessoa
+    // private String cpf; // vai pra pessoa
+    // private String endereco; // vai pra endereco
+    // private int matricula; // // vai pra pessoa
+    // private String dataNascimento; // vai pra pessoa
+    // private String genero; // vai pra pessoa
+    //private float peso; // vai pra avaliacao
+    //private float altura; // vai pra avaliacao
+    //private String email; // vai pra contato
+    //private List<String> telefones; tem q excluir
+    // private float imc; // vai pra avaliacao
+    // private Plano plano; // vai pra assinatura
+
+    public Aluno(){
     }
+
+   
     
-    public String getEndereco(){
-        return endereco; 
-    }
-    
-    public int getMatricula(){
-        return matricula; 
-    }
-    
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-    
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
-
-    public float getAltura() {
-        return altura;
-    }
-
-    public List<String> getTelefones() {
-        return telefones;
-    }
-
-    public float getImc() {
-        return imc;
-    }
-
-    public void setAltura(float altura) {
-        this.altura = altura;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPlano(Plano plano){
-        if (this.plano != plano){
-            plano.addAluno(this);
-        }
-        this.plano = plano;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setImc() {
-        imc = this.peso / (this.altura * this.altura);
-    }
-
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-
-    public void addTelefone(String telefone){
-        this.telefones.add(telefone);
-    }
-
-    public void cadastro(){
-        //sda 
-    }
-
-    public void CriarFichaTreino(){
-        //
-    }
 }
--- demo/src/main/java/br/cefet/Pessoa.java
+++ demo/src/main/java/br/cefet/Pessoa.java
@@ -1,5 +1,15 @@
 package br.cefet;
 
-public class Pessoa {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private int matricula;
+    private String dataNascimento;
+    private String genero;
+    private List<Contato> contatos;
+    private Endereco endereco;
+    private List<Documento> documentos;   
 }
