511ff9eab4c76566785073b782fb6391fb633af1
feefsan <twelve.feh@gmail.com>
2025-05-05 22:28:19 -0300

    Improve tests

--- demo/src/main/java/br/cefet/Main.java
+++ demo/src/main/java/br/cefet/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.printf("\nNome: %s", pessoa.getNome());
         System.out.printf("\nData nascimento: %s", pessoa.getDataNascimento());
         System.out.printf("\nMatricula: %s", pessoa.getMatricula());
+        
         System.out.printf("\nGenero: %s", pessoa.getGenero());
 
         for(Documento documento: pessoa.getDocumentos()){
@@ -35,12 +36,12 @@ public class Main {
         Random rand1 = new Random();
         int matricula = rand1.nextInt(200);
 
-        Atendente at1 = new Atendente(null, matricula, null, matricula, nomes[sortearNumero(nomes)], null, null, null, null, null, null);
+        Atendente at1 = new Atendente(TipoTurno.MANHA, matricula, "Masculino", 1952, nomes[sortearNumero(nomes)], "Recepção", "22/03/1998", TipoContrato.CLT, "Ativo", "30/04/2024", "1234567/8910");
         at1.addDocumento(new Documento(TipoDocumento.RG, "31.728.312-5"));
         at1.addContato(new Contato(TipoContato.INSTAGRAM, "@o_lie"));
         at1.setEndereco(new Endereco("Rua Apinajé", 32, "Vila", "Engenho da Rainha", "Rio de Janeiro", "20766-590", "RJ"));
 
-        Treinador t1 = new Treinador(matricula, null, false, null, matricula, nomes[sortearNumero(nomes)], null, null, null, null, null, null);
+        Treinador t1 = new Treinador(matricula, "RJ456214", true, "Feminino", 3020, nomes[sortearNumero(nomes)], "Treinador", "03/02/1995", TipoContrato.PJ, "Ativo", "03/02/2022", "4565217/2105");
         t1.addDocumento(new Documento(TipoDocumento.CNH, "145.026.552-03"));
         t1.addContato(new Contato(TipoContato.TELEFONE, "(21) 97623-5484"));
         t1.setEndereco(new Endereco("Rua Bororó", 50, "-", "Engenho da Rainha", "Rio de Janeiro", "20766-590", "RJ"));
@@ -54,23 +55,6 @@ public class Main {
         a1.setAssinatura(new Assinatura(a1, "01/02/2025", "01/02/2026", new Plano(115, "Cartao Credito", TipoPlano.ANUAL, "01/02/2025", "Final da assinatura")));
         a1.addAvaliacao(new Avaliacao("01/02/2025", t1, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, matricula, false, false, false, null, false));
 
-        // testes
-        /*System.out.printf("\nNome: %s", a1.getNome());
-        System.out.printf("\nData nascimento: %s", a1.getDataNascimento());
-        System.out.printf("\nMatricula: %s", a1.getMatricula());
-        System.out.printf("\nGenero: %s", a1.getGenero());
-
-        for(Documento documento: a1.getDocumentos()){
-            System.out.printf("\nTipo de documento: %s - Conteudo: %s", documento.getTipo(), documento.getConteudo());
-        }
-
-        for(Contato contato: a1.getContatos()){
-            System.out.printf("\nTipo de contato: %s - Conteudo: %s", contato.getTipo(), contato.getConteudo());
-        }
-
-        System.out.printf("\nEndereço: %s %d %s - %s, %s, %s - CEP: %s", a1.getEndereco().getLogradouro(), a1.getEndereco().getNumero(), a1.getEndereco().getComplemento(), a1.getEndereco().getBairro(), a1.getEndereco().getCidade(), a1.getEndereco().getEstado(), a1.getEndereco().getCep());
-
-        System.out.println();*/
         testarPessoa(a1);
         System.out.println();
         testarPessoa(at1);
